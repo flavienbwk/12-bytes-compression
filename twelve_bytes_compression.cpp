b3a2e2b3b8c1ee736c322b182c17a66fe0ac4280
flavienbwk <flavienbwk>
2020-08-28 17:09:38 +0200

    Updated date and version of the algorithm

--- twelve_bytes_compression.cpp
+++ twelve_bytes_compression.cpp
@@ -2,8 +2,8 @@
  * @file twelve_bytes_compression.cpp
  * @author Flavien Berwick (flavien@berwick.fr)
  * @brief Algorithm for compressing digital values for SigFox frames of 12 bytes.
- * @version 0.1
- * @date 2019-06-17
+ * @version 0.2
+ * @date 2020-08-28
  * 
  * Under MIT license. See the full license here :
  * https://github.com/flavienbwk/12-bytes-compression
